a6fb05756d6d148cf3747fe2d1c0ce482fdef9aa
kadify <kadify.dev@gmail.com>
2025-04-29 13:10:51 +0300

    fix(auth): изменена продолжительность refresh токена на дни

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -73,5 +73,5 @@ class AuthService:
     def create_refresh_token(user_id: int) -> str:
         return create_token(
             {"sub": str(user_id), "type": "refresh"},
-            timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_DAYS),
+            timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS),
         )
