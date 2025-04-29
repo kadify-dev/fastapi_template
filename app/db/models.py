7950df02d7bf9807ee9c3765fe88550038ec0546
kadify <kadify.dev@gmail.com>
2025-04-29 21:00:54 +0300

    feat(models): добавлена role для User

--- app/db/models.py
+++ app/db/models.py
@@ -1,9 +1,15 @@
 import uuid
+from enum import StrEnum
 
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.database import Base
+
+
+class UserRole(StrEnum):
+    USER = "USER"
+    ADMIN = "ADMIN"
 
 
 class User(Base):
@@ -14,6 +20,7 @@ class User(Base):
     )
     email: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
+    role: Mapped[UserRole] = mapped_column(default=UserRole.USER, server_default='USER')
 
     def __repr__(self):
         return f"<User id={self.id} email={self.email}>"
